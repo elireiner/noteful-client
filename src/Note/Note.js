58681a6f59721034f47f4a41b79691452c01f521
Eli Reiners <goelyukim@gmail.com>
2020-07-13 20:57:05 -0400

    Initial commit

---
+++ src/Note/Note.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import ApiContext from '../ApiContext'
+import config from '../config'
+import './Note.css'
+
+export default class Note extends React.Component {
+  static defaultProps = {
+    onDeleteNote: () => { },
+  }
+  static contextType = ApiContext;
+
+  handleClickDelete = e => {
+    e.preventDefault()
+    const noteId = this.props.id
+console.log(this.props)
+    fetch(`${config.API_ENDPOINT}/notes/${noteId}`, {
+      method: 'DELETE',
+      headers: {
+        'content-type': 'application/json'
+      },
+    })
+      .then(res => {
+        if (!res.ok)
+          return res.json().then(e => Promise.reject(e))
+        return res.json()
+      })
+      .then(() => {
+        this.context.deleteNote(noteId)
+        // allow parent to perform extra behaviour
+        this.props.onDeleteNote(noteId)
+      })
+      .catch(error => {
+        console.error({ error })
+      })
+  }
+
+  render() {
+    const { note_name, id } = this.props
+    console.log(this.props)
+    return (
+      <div className='Note'>
+        <h2 className='Note__title'>
+          <Link to={`/note/${id}`}>
+            {note_name}
+          </Link>
+        </h2>
+        <button
+          className='Note__delete'
+          type='button'
+          onClick={this.handleClickDelete}
+        >
+          <FontAwesomeIcon icon='trash-alt' />
+          {' '}
+          remove
+        </button>
+      </div>
+    )
+  }
+}
---
+++ src/notes-helpers.js
@@ -0,0 +1,18 @@
+
+export const findFolder = (folders=[], folderId) =>
+  folders.find(folder => folder.id === folderId)
+
+export const findNote = (notes=[], noteId) => {
+   notes.find(note => note.id === noteId)
+   console.log('i ran', noteId, notes.find(note => note.id === noteId))
+}
+ 
+
+export const getNotesForFolder = (notes=[], folderId) => (
+  (!folderId)
+    ? notes
+    : notes.filter(note => note.folder === folderId)
+)
+
+export const countNotesForFolder = (notes=[], folderId) =>
+  notes.filter(note => note.folder === folderId).length
