02c8a125dbdd78eaadfe329dac2b4a305b6566ed
Eli Reiners <goelyukim@gmail.com>
2020-07-22 15:56:23 -0400

    Add authorization

--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -21,7 +21,8 @@ export default class AddNote extends Component {
     fetch(`${config.API_ENDPOINT}/notes`, {
       method: 'POST',
       headers: {
-        'content-type': 'application/json'
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${config.API_KEY}`
       },
       body: JSON.stringify(newNote),
     })
